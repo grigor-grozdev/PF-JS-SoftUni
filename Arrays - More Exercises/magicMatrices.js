2df1e65bbd3417136b855e631aac9100e8adb815
Grigor Grozdev <grigor.grozdev@gmail.com>
2023-10-01 20:39:27 +0300

    update  magicMatrices

--- Arrays - More Exercises/magicMatrices.js
+++ Arrays - More Exercises/magicMatrices.js
@@ -1,14 +1,35 @@
-function magicMatrices(arr){
- for (let i = 0; i < arr.length; i++){
-    let sum = 0;
-    for (let j = 0; j < arr[i].length; j++){
-        sum += arr[i][j];
+function magicMatrices(arr) {
+    let initialSum = 0;
+    for (let num of arr[0]) {
+        initialSum += num;
     }
-    console.log(sum)
- }
+
+    let isMagic = true;
+
+    for (let i = 0; i < arr.length; i++) {
+        let sumLine = 0;
+        for (let j = 0; j < arr[i].length; j++) {
+            sumLine += arr[i][j];
+        }
+        if (sumLine != initialSum) {
+            isMagic = false;
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        let sumRow = 0;
+        for (let j = 0; j < arr.length; j++) {
+            sumRow += arr[j][i];
+        }
+        if (sumRow != initialSum) {
+            isMagic = false;
+        }
+    }
+
+    console.log(isMagic);
 
 }
 
-magicMatrices([[4, 5, 6],
-    [6, 5, 4],
-    [5, 5, 5]])
+magicMatrices([[1, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0]])
