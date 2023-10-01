66398596216df5f5844ac7892fc761f861031bdd
Grigor Grozdev <grigor.grozdev@gmail.com>
2023-10-01 08:46:17 +0300

    adding 1 exercise

--- Arrays - Exercise/dungeonestDark.js
+++ Arrays - Exercise/dungeonestDark.js
@@ -1,11 +1,52 @@
-function dungeonestDark(arr){
+function dungeonestDark(arr) {
     let health = 100;
     let coins = 0;
-    
-    
-    let roomArr = arr.split('|');
-    console.log(roomArr);
-    
+
+    let roomsInfo = arr[0];
+    let roomArr = roomsInfo.split('|');
+
+    for (let i = 0; i < roomArr.length; i++) {
+        let room = roomArr[i];
+        let roomSpl = room.split(' ');
+
+        let item = roomSpl[0];
+        let num = Number(roomSpl[1]);
+        let hp =0
+
+        if (item == "potion") {
+            health += num;
+            if (health > 100) {
+                hp = num - (health-100);
+                health = 100;
+            } else {
+                hp = num;
+            }
+
+            console.log(`You healed for ${hp} hp.`);
+            console.log(`Current health: ${health} hp.`);
+        } else if (item == "chest") {
+            coins += num;
+            console.log(`You found ${num} coins.`);
+        } else {
+            health -= num;
+            if (health > 0) {
+                console.log(`You slayed ${item}.`);
+            } else {
+                console.log(`You died! Killed by ${item}.`);
+                console.log(`Best room: ${i + 1}`);
+                break;
+            }
+
+        }
+
     }
-    
-    dungeonestDark(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"]);
+    if (health > 0) {
+        console.log(`You've made it!`);
+        console.log(`Coins: ${coins}`);
+        console.log(`Health: ${health}`);
+    }
+
+
+}
+
+dungeonestDark(["cat 10|potion 30|orc 10|chest 10|snake 25|chest 110"]);
---
+++ Arrays - Exercise/ldyBugs.js
@@ -0,0 +1,62 @@
+function ldyBugs(arr) {
+    let fieldCount = arr.shift();
+    let field = [];
+    let ladybugs = arr.shift();
+    let ladybugsOnField = ladybugs.split(' ');
+
+    for (let index of ladybugsOnField) {
+        if (index >= 0 && index < fieldCount) {
+            field[index] = 1;
+        }
+    }
+
+    for (let i = 0; i < fieldCount; i++) {
+        if (field[i] != 1) {
+            field[i] = 0;
+        }
+    }
+
+   // console.log(field)
+
+    for (let input of arr) {
+        let flight = input.split(' ');
+        let start = Number(flight[0]);
+        let moves = Number(flight[2]);
+        let direction = flight[1];
+        if (start < 0 || start >= fieldCount) {
+            break;
+        } else {
+        field[start] = 0;
+        }
+
+        switch (direction) {
+            case 'right':
+                let endR = start + moves;
+                for (let j = endR; j >= 0 && j < fieldCount; j += moves) {
+                    if (j >= fieldCount || j < 0) {
+                        break;
+                    } else if (field[j] == 0) {
+                        field[j] = 1;
+                        break;
+                    }
+                }
+                break;
+            case 'left':
+                let endL = start - moves;
+                for (let j = endL; j >= 0 && j < fieldCount; j -= moves) {
+                    //if (j >= fieldCount || j < 0) {
+                    //    break;
+                    //} else 
+                    if (field[j] == 0) {
+                        field[j] = 1;
+                        break;
+                    }
+                }
+                break;
+        }
+    }
+    console.log(field.join(' '));
+
+}
+
+ldyBugs([ 3, '0 1', '0 right 1', '2 right 1', '1 right -2' ]);
