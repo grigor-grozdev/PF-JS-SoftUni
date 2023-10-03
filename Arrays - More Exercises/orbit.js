05923f250748802f47e5871d7aaf7508badb45f9
Grigor Grozdev <grigor.grozdev@gmail.com>
2023-10-03 08:13:33 +0300

    update

--- Arrays - More Exercises/orbit.js
+++ Arrays - More Exercises/orbit.js
@@ -11,27 +11,55 @@ function orbit(input) {
     }
     arr[x][y] = 1;
 
-let num = 2;
-let startRow = x+1;
-let startColumn = y+1;
-//let endRow = x+1;
-//let endColumn = y+1;
+    let num = 2;
+    let startRow = y - 1;
+    let startColumn = x - 1;
+    let endRow = y + 1;
+    let endColumn = x + 1;
 
-while (startRow < height && startColumn < width){
+    while ((startRow >= 0 || endRow < height) && (startColumn >= 0 || endColumn < width)) {
+        if (startRow >= 0) {
+            for (let j = startColumn; j <= endColumn; j++) {
+                arr[startRow][j] = num;
+            }
+        } else {
+            startRow = 0;
+        }
 
-for (let j = 0; j <= startColumn; j++ ){
-    arr[startRow][j] = num;
-    arr[j][startColumn] = num;
+        if (endColumn < width) {
+            for (let k = startRow; k <= endRow; k++) {
+                arr[k][endColumn] = num;
+            }
+        } else {
+            endColumn = width - 1;
+        }
+
+        if (endRow < height) {
+            for (let l = endColumn; l >= startColumn; l--) {
+                arr[endRow][l] = num;
+            }
+        } else {
+            endRow = height - 1;
+        }
+
+        if (startColumn >= 0) {
+            for (let m = endRow; m >= startRow; m--) {
+                arr[m][startColumn] = num;
+            }
+        } else {
+            startColumn = 0;
+        }
+        num++;
+        startRow--
+        endRow++
+        startColumn--
+        endColumn++
     }
-num++;
-startRow++;
-startColumn++;
+
+    for (let p = 0; p < arr.length; p++) {
+        console.log(arr[p].join(' '));
+    }
 }
 
-for (let p = 0; p < arr.length; p++) {
-    console.log(arr[p].join(' '));
-}
-}
-
-orbit([7, 7,
-    0, 0])
+orbit([5, 5,
+    4, 4])
